46721020d924dd86de3fb5f03070a1ecf2c826fd
Niklas <inf3929@hs-worms.de>
2022-11-10 00:29:27 +0100

    added more basic functions

--- game_api.py
+++ game_api.py
@@ -1,7 +1,15 @@
 
+from argparse import ArgumentError
+from copy import deepcopy
+import time
+from typing import Iterable
 import requests
 import json 
+from googletrans import Translator
 class trivia():
+    translation = Translator()
+    _TOKEN = None
+    _categories = {}
     #{"response_code":0,
     # "results":
     # [ 
@@ -16,13 +24,111 @@ class trivia():
     class game():
         players = []    #player = ("name",points)
         questions = []  #question = (question,correct answer,incorrect_answers)
-    def __init__():
-        pass
+        settings = {'amount': 0,'typ': 'n','catergories':[],'difficulty':'n'} #amount,typ,category,difficulty
+        # category is a list and could be selected from with a random function or via asking the players == > buildURL must be called once for each category
+    
+    def __init__(self):
+        resp= requests.get("https://opentdb.com/api_token.php?command=request").json()
+        self._TOKEN = resp['token']
+        self.buildCategories()
+        self._game = self.game()
+        url = self.buidlUrl(
+                            self._game.settings["amount"],
+                            self._game.settings["typ"],
+                            self._game.settings["categories"],
+                            self._game.settings["difficulty"]
+                            )
+        self._game.questions = self.getQuestions(url)
 
-    def buidlUrl(amount,typ,categories,difficulty):
-        comp = [typ,categories,difficulty]
-        comp.append("https://opentdb.com/api.php?")
-        url = "&".join(comp)
+
+        self.buidlUrl
+    def setAmount(self,amount:int):
+        if amount <= 0:
+            raise ArgumentError(message="amount must not be lower than 0 or bigger than 50")
+        self._game.settings["amount"] = amount
+
+    def setTyp(self,typ: str):
+        if typ == "mc":
+            self._game.settings["typ"] = "mc"
+        elif typ == "tf":
+            self._game.settings["typ"] = "tf"
+        else:
+            raise ArgumentError.message("typ must be either mc or tf")
+    
+    def setCategory(self,category:int,islist = False):
+        if islist:
+            for cate in category:
+                if category > 32 or category < 9:
+                    raise ArgumentError(message="category must be a number between 9 and 32")
+            for cate in category:
+                self._game.settings["categories"].append(category)
+        else:
+            if category > 32 or category < 9:
+                raise ArgumentError(message="category must be a number between 9 and 32")
+            self._game.settings["categories"].append(category)
+
+    def setDifficulty(self,difficulty:int):
+        if difficulty > 3 or difficulty < 1:
+            raise ArgumentError(message="Difficulty must be a number between 1-3")
+        self._game.settings["difficulty"] = difficulty
+
+    def buildCategories(self):
+        categoryResponse = requests.get("https://opentdb.com/api_category.php").json()
+        self._categories = deepcopy(categoryResponse['trivia_categories'])
+        return self._categories
+    def getQuestions(self,url):
+        resp = requests.get(url).json()
+        quest = []
+        if resp["response_code"] != 0:
+            return
+        for question in resp['results']:
+            print("ENG:  "+question['question'])
+            quest.append(question['question'])
+            #print("GER:  "+self.translation.translate(str(question['question'])))
+
+          
+
+
+    def buidlUrl(self,amount: int,typ: str,category: int,difficulty: str or int):
+        if difficulty > 3 or difficulty < 1:
+            raise ArgumentError(message="Difficulty must be a number between 1-3")
+        if amount <= 0:
+            raise ArgumentError(message="amount must not be lower than 0 or bigger than 50")
+        if category > 32 or category < 9:
+            raise ArgumentError(message="category must be a number between 9 and 32")
+        comp = []
+
+        # Amount 
+        amount = str(amount)
+        comp.append("amount="+amount)
+
+        #Typ
+        if typ == "mc":
+            comp.append("type=multiple")
+        elif typ == "tf":
+            comp.append("type=boolean")
+        else:
+            raise ArgumentError.message("typ must be either mc or tf")
+
+        # Categories
+        comp.append("category="+str(category))
+        
+        # Difficulty
+        if "str" in str(type(difficulty)):
+            difficulty = "difficulty="+difficulty
+        else:
+            if difficulty == 1:
+                difficulty = "difficulty=easy" 
+            elif difficulty == 2:
+                difficulty = "difficulty=medium"
+            elif difficulty == 3:
+                difficulty == "difficulty=hard"
+        comp.append(difficulty)
+
+
+        url = "https://opentdb.com/api.php?"+( "&".join(comp))
+        tokenstring = "&token="+str(self._TOKEN)
+        url = url + tokenstring
         return url
 
     #difficulty = "" # easy,medium,hard or mixed or 1,2,3
@@ -32,3 +138,4 @@ class trivia():
             return 
         result = json.loads(resp.json)
         questions = result["results"]
+g = trivia()
