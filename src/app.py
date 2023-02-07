b17802515cefec2ab6959a28f1cf4b2f905aca62
Felicitas Fuhrmann <lukutaki@outlook.com>
2023-02-07 19:10:07 +0100

    new categories

--- src/app.py
+++ src/app.py
@@ -75,100 +75,130 @@ logger.addHandler(handler)
 CAT_DATASOURCE = {
     "trivia_categories": [
         {
-            "id": 9,
-            "name": "General Knowledge"
-        },
-        {
-            "id": 10,
-            "name": "Entertainment: Books"
-        },
-        {
-            "id": 11,
-            "name": "Entertainment: Film"
-        },
-        {
-            "id": 12,
-            "name": "Entertainment: Music"
-        },
-        {
-            "id": 13,
-            "name": "Entertainment: Musicals & Theatres"
-        },
-        {
-            "id": 14,
-            "name": "Entertainment: Television"
-        },
-        {
-            "id": 15,
-            "name": "Entertainment: Video Games"
-        },
-        {
-            "id": 16,
-            "name": "Entertainment: Board Games"
-        },
-        {
-            "id": 17,
-            "name": "Science & Nature"
-        },
-        {
-            "id": 18,
-            "name": "Science: Computers"
-        },
-        {
-            "id": 19,
-            "name": "Science: Mathematics"
-        },
-        {
-            "id": 20,
-            "name": "Mythology"
-        },
-        {
-            "id": 21,
-            "name": "Sports"
-        },
-        {
-            "id": 22,
-            "name": "Geography"
-        },
-        {
-            "id": 23,
-            "name": "History"
-        },
-        {
-            "id": 24,
-            "name": "Politics"
-        },
-        {
-            "id": 25,
-            "name": "Art"
-        },
-        {
-            "id": 26,
-            "name": "Celebrities"
-        },
-        {
-            "id": 27,
-            "name": "Animals"
-        },
-        {
-            "id": 28,
-            "name": "Vehicles"
-        },
-        {
-            "id": 29,
-            "name": "Entertainment: Comics"
-        },
-        {
-            "id": 30,
-            "name": "Science: Gadgets"
-        },
-        {
-            "id": 31,
-            "name": "Entertainment: Japanese Anime & Manga"
-        },
-        {
-            "id": 32,
-            "name": "Entertainment: Cartoon & Animations"
+            "sub_cats": [
+                {
+                    "name": "Geography"
+                },
+                {
+                    "name": "History"
+                },
+                {
+                    "name": "Sports & Hobbies"
+                },
+                {
+                    "name": "Art"
+                },
+                {
+                    "name": "Entertainment"
+                },
+                {
+                    "name": "Science"
+                },
+                {
+                    "Geography": [
+                        {
+                            "id": 22,
+                            "name": "Geography"
+                        }
+                    ]
+                },
+                {
+                    "History": [
+                        {
+                            "id": 23,
+                            "name": "History"
+                        }
+                    ]
+                },
+                {
+                    "Sports": [
+                        {
+                            "id": 21,
+                            "name": "Sports"
+                        },
+                        {
+                            "id": 28,
+                            "name": "Vehicles"
+                        }
+                    ]
+                },
+                {
+                    "Art": [
+                        {
+                            "id": 25,
+                            "name": "Art"
+                        }
+                    ]
+                },
+                {
+                    "Entertainment": [
+                        {
+                            "id": 10,
+                            "name": "Entertainment: Books"
+                        },
+                        {
+                            "id": 11,
+                            "name": "Entertainment: Film"
+                        },
+                        {
+                            "id": 12,
+                            "name": "Entertainment: Music"
+                        },
+                        {
+                            "id": 13,
+                            "name": "Entertainment: Musicals & Theatres"
+                        },
+                        {
+                            "id": 14,
+                            "name": "Entertainment: Television"
+                        },
+                        {
+                            "id": 15,
+                            "name": "Entertainment: Video Games"
+                        },
+                        {
+                            "id": 16,
+                            "name": "Entertainment: Board Games"
+                        },
+                        {
+                            "id": 29,
+                            "name": "Entertainment: Comics"
+                        },
+                        {
+                            "id": 31,
+                            "name": "Entertainment: Japanese Anime & Manga"
+                        },
+                        {
+                            "id": 32,
+                            "name": "Entertainment: Cartoon & Animations"
+                        },
+                        {
+                            "id": 26,
+                            "name": "Celebrities"
+                        }
+                    ]
+                },
+                {
+                    "Science": [
+                        {
+                            "id": 17,
+                            "name": "Science & Nature"
+                        },
+                        {
+                            "id": 18,
+                            "name": "Science: Computers"
+                        },
+                        {
+                            "id": 19,
+                            "name": "Science: Mathematics"
+                        },
+                        {
+                            "id": 30,
+                            "name": "Science: Gadgets"
+                        }
+                    ]
+                }
+            ]
         }
     ]
 }
